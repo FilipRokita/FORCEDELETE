4b13cca33daf072025845bf18126e93436950e80
Filip Rokita <filiprokita4@gmail.com>
2021-12-05 01:55:22 +0100

    changed dir

---
+++ FORCEDELETE/FORCEDELETE.cpp
@@ -0,0 +1,28 @@
+﻿//FORCEDELETE
+//Filip Rokita
+//www.filiprokita.com
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string directory;
+string command;
+string quotationmark = "\"";
+string commandrd;
+
+int main()
+{
+	system("title FORCEDELETE - www.filiprokita.com");
+
+	cout << "Path: ";
+	getline(cin, directory);
+	cout << endl;
+
+	command = quotationmark + "del /f " + quotationmark + directory + quotationmark + quotationmark;
+	system(command.c_str());
+	system("pause");
+
+	return 0;
+}
